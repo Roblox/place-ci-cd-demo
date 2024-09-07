53659ed0823419887da30e96f3ca02ce538d2d32
Lucas Li <lucas.li@roblox.com>
2024-09-07 08:55:49 -0700

    update user id

--- scripts/lua/task.lua
+++ scripts/lua/task.lua
@@ -33,7 +33,7 @@ local function publish_package_asset(instance)
 	local as = game.AssetService
 
 	local requestParameters = {
-		CreatorId = 1253904491,
+		CreatorId = 1253904492,
 		CreatorType = Enum.AssetCreatorType.User,
 		Name = "Today",
 		Description = "hack your game",
