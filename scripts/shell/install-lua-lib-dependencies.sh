9da09364b3c89bf335b622271729d1ef33d511bf
Lucas Li <lucas.li@roblox.com>
2024-09-06 17:46:49 -0700

    apt install luarocks

--- scripts/shell/install-lua-lib-dependencies.sh
+++ scripts/shell/install-lua-lib-dependencies.sh
@@ -4,27 +4,27 @@
 sudo apt-get update
 
 # Install Lua and its development files
-sudo apt-get install -y lua5.3 liblua5.3-dev
+sudo apt-get install -y lua5.3 liblua5.3-dev luarocks
 
-# Install other required dependencies
-sudo apt-get install -y build-essential libreadline-dev unzip
+# # Install other required dependencies
+# sudo apt-get install -y build-essential libreadline-dev unzip
 
-# Set LuaRocks version
-LUAROCKS_VERSION="3.9.2"
+# # Set LuaRocks version
+# LUAROCKS_VERSION="3.9.2"
 
-# Download LuaRocks
-wget "https://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz"
+# # Download LuaRocks
+# wget "https://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz"
 
-# Extract the archive
-tar zxpf "luarocks-$LUAROCKS_VERSION.tar.gz"
+# # Extract the archive
+# tar zxpf "luarocks-$LUAROCKS_VERSION.tar.gz"
 
-# Change to LuaRocks directory
-cd "luarocks-$LUAROCKS_VERSION"
+# # Change to LuaRocks directory
+# cd "luarocks-$LUAROCKS_VERSION"
 
-# Configure, build, and install LuaRocks
-./configure --with-lua-include=/usr/include/lua5.3
-make
-sudo make install
+# # Configure, build, and install LuaRocks
+# ./configure --with-lua-include=/usr/include/lua5.3
+# make
+# sudo make install
 
 # Verify the installation
 luarocks --version
